a0eed713b499a8a313a376b2fc4a1c3285e589cc
Ahmed Ezz <95645898+A7med3z@users.noreply.github.com>
2022-11-12 22:53:02 +0200

    Update AlphaBeta.java

--- src/main/java/src/AlphaBeta.java
+++ src/main/java/src/AlphaBeta.java
@@ -4,13 +4,13 @@ import static src.GamePlay.*;
 
 public class AlphaBeta {
 
-    static GamePlay.Pair maximize(StateNode stateNode, double alpha, double beta, int k) {
+    static GamePlay.Pair maximize(StateNode stateNode, int alpha, int beta, int k) {
         if (k <= 0) return new GamePlay.Pair(stateNode, utility(stateNode));
         GamePlay.Pair pair = new GamePlay.Pair(null, -1 * OO);
         for (StateNode child : makeChildrenReady(stateNode)) {
-            int utility = minimize(child, alpha, beta, k - 1).val;
-            if (utility > pair.val)
-                pair = new GamePlay.Pair(child, utility);
+            int utl = minimize(child, alpha, beta, k - 1).val;
+            if (utl > pair.val)
+                pair = new GamePlay.Pair(child, utl);
             if (pair.val >= beta)
                 break;
             if (pair.val > alpha)
@@ -19,13 +19,15 @@ public class AlphaBeta {
         return pair;
     }
 
-    static GamePlay.Pair minimize(StateNode stateNode, double alpha, double beta, int k) {
+    static GamePlay.Pair minimize(StateNode stateNode, int alpha, int beta, int k) {
         if (k <= 0) return new GamePlay.Pair(stateNode, utility(stateNode));
         GamePlay.Pair pair = new GamePlay.Pair(null, OO);
         for (StateNode child : makeChildrenReady(stateNode)) {
-            int utility = maximize(child, alpha, beta, k - 1).val;
-            if (utility < pair.val)
-                pair = new GamePlay.Pair(child, utility);
+            int utl = maximize(child, alpha, beta, k - 1).val;
+            if (utl < pair.val) {
+                pair.state = child;
+                pair.val = utl;
+            }
             if (pair.val <= alpha)
                 break;
             if (pair.val < beta)
